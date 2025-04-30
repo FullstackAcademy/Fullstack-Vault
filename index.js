6ca620ca846f26d74b5940b8fb21b8b02e1696d2
MrJoshReeder <mr.reeder25@gmail.com>
2025-04-30 15:51:18 -0600

    Here are my updated files

---
+++ index.js
@@ -0,0 +1,14 @@
+let code1
+code1 = 7 + 3;
+let code2
+code2 = 48 - 16 / 2;
+let code3
+code3 = 39 % 40;
+let message = "The vault has been sacured. The combination is:";
+let codeA
+codeA = "10-40-39";
+let codeB 
+codeB = '10-40-39';
+console.log (message);
+console.log (codeA);
+console.log (codeB);
